6320b90d4ff237030039d5c38759066a7ce4f679
Abdullah al rafi <abdullah116632@gmail.com>
2024-11-20 15:40:11 +0600

    atlas setup & basic CRUD

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 
 
@@ -10,7 +12,16 @@ const port = 4000;
 
 //middleware
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cors())
+
+// db connection
+connectDB();
+
+
+//api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"))
 
 app.get("/", (req, res)=>{
     res.send("api working")
