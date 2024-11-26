955d4911e3ca8b1508271d402319f44a389d6f5a
Abdullah al rafi <abdullah116632@gmail.com>
2024-11-26 22:41:26 +0600

    user order page

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js"
-import { placeOrder, verifyOrder } from "../controllers/orderController.js";
+import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
 
 const orderRoute = express.Router();
 
 orderRoute.post("/place", authMiddleware, placeOrder);
 orderRoute.post("/verify", verifyOrder);
+orderRoute.post("/userorders", authMiddleware, userOrders)
 
 export default orderRoute;
